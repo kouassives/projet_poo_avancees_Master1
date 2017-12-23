c0bb9c882e3cb316ec696e452366c8d8b173c662
kouassives <kouassives@gmail.com>
2017-12-23 03:27:50 +0000

    Creation fenetre connexion et ajouter de resources images

--- src/gest/MainApp.java
+++ src/gest/MainApp.java
@@ -1,21 +1,57 @@
 package gest;
 
-import java.sql.SQLException;
-import java.util.Scanner;
+import java.io.IOException;
 
-import connection.ControleConnexion;
-import connection.TestRequetes;
+import gest.view.FenConnexionController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+	
+	public MainApp() {
+    }
+
 	@Override
-	public void start(Stage primaryStage) throws SQLException {
-		
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Gestion");
+
+        // Set the application icon.
+       //this.primaryStage.getIcons().add(new Image("resources/images/icone_eclipse.ico"));
+
+        initPrimaryLayout();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	public void initPrimaryLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/FenConnexion.fxml"));
+            AnchorPane FenConnexion = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(FenConnexion);
+            primaryStage.setScene(scene);
+            
+            // Give the controller access to the main app.
+            FenConnexionController controller = loader.getController();
+            controller.setMainApp(this);
+            
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
 }
+
--- src/gest/view/FenConnexionController.java
+++ src/gest/view/FenConnexionController.java
@@ -1,5 +1,47 @@
 package gest.view;
 
+import gest.MainApp;
+import javafx.fxml.FXML;
+
 public class FenConnexionController {
+	
+	 // Reference to the main application
+    private MainApp mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+    
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public FenConnexionController() {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    	
+    }
+    
+    @FXML
+    private void handleValider() {
+    	
+    }
+    
+    @FXML
+    private void handleQuitter() {
+    	System.exit(0);
+    }
+
 
 }
