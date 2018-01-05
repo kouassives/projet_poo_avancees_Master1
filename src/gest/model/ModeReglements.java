836a8aebf8f26699b7ea538c5053c31a9f17e780
kouassives <kouassives@gmail.com>
2018-01-05 04:18:29 +0000

    importation des biblothèque dans le dossiber lib

---
+++ src/gest/model/ModeReglements.java
@@ -0,0 +1,68 @@
+package gest.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class ModeReglements implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+    private int code;
+	
+	@Basic
+    private String type;
+    
+    @Transient
+    private transient boolean ignor�;
+    
+    /*
+     * Constructeur 1.
+     * Utilisé par JPA.
+     */
+    public ModeReglements(){
+    	super();
+    }
+    
+    /*
+     * Constructeur 2
+     * Pour la gestion des commandes
+     */
+    public ModeReglements(String type){
+    	this();
+    	this.type = type;
+    }
+
+    /*
+     * Accesseurs
+     */
+    public int getCode() {
+        return code;
+    }
+    public String getType() {
+        return type;
+    }
+    
+    /*
+     * Mutateur
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Décrit le mode de règlement
+     * de manière textuelle.
+     */
+    @Override
+    public String toString() {
+    	return type;
+    }
+}
